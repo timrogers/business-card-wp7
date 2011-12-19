b17b37964554b1267b30fa714a71bd78e3013850
Tim Rogers <tim@tim-rogers.co.uk>
2011-12-19 18:23:04 +0000

    First commit, as requested by Alex

---
+++ Tim Rogers/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Tim_Rogers
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void image2_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void GestureListener_Tap(object sender, Microsoft.Phone.Controls.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Back.xaml", UriKind.Relative));
+        }
+    }
+}
